fb1c22b71d3414917bd5887462873275ea139d5d
skyywhyy <den4ik.barakin@gmail.com>
2024-10-25 13:49:28 +0300

    добавил функцию которая регистрирует клики игрока и размещает символы на игровом поле

--- tic tac game.py
+++ tic tac game.py
@@ -1,5 +1,33 @@
 import tkinter as tk
 
+def draw_symbol(row, col):
+    x_center = col * cell_size + cell_size // 2
+    y_center = row * cell_size + cell_size // 2
+    symbol = game_board[row][col]
+    if symbol == "X":
+        board.create_text(x_center, y_center, text="X", font=("Arial", 24), fill="blue")
+    elif symbol == "O":
+        board.create_text(x_center, y_center, text="O", font=("Arial", 24), fill="red")
+
+def switch_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
+
+def on_cell(event):
+    global counter
+    x, y = event.x, event.y
+    col = x // cell_size
+    row = y // cell_size
+    if game_board[row][col] == " ":
+        game_board[row][col] = current_player
+        draw_symbol(row, col)
+        counter += 1
+        switch_player()
+
+
 #окно игры
 window = tk.Tk()
 window.title("tic-tac-toe")
@@ -20,3 +48,6 @@ for row in range(3):
         board.create_rectangle(x1, y1, x2, y2)
 
 window.mainloop()
+
+board.bind("<Button-1>", on_cell)
+
