16a656c3b9bc76afa37681400acf287dff5764d4
skyywhyy <den4ik.barakin@gmail.com>
2024-10-25 10:23:25 +0300

    создал пустое окно с игровым полем

---
+++ tic tac game.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+#окно игры
+window = tk.Tk()
+window.title("tic-tac-toe")
+
+#игровое поле (пустая сетка)
+cell_size = 100
+cell_otst = 5
+board = tk.Canvas(window, width=300, height=300)
+board.pack()
+
+# Создание пустой сетки
+for row in range(3):
+    for col in range(3):
+        x1 = col * cell_size + cell_otst 
+        y1 = row * cell_size + cell_otst 
+        x2 = x1 + cell_size - cell_otst 
+        y2 = y1 + cell_size - cell_otst 
+        board.create_rectangle(x1, y1, x2, y2)
+
+window.mainloop()
